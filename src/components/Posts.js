75653889e362875ff675a173aab2806f654daa65
KasiaTorz <ktorz@wp.pl>
2018-04-09 17:45:18 +0200

    build constructor to put states

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 
 class Posts extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            posts:[]
+        }
+
+    }
+
     componentWillMount(){
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(res => res.json())

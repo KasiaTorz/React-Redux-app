8a0e5496c84fcaf3963ed472636c48232506ad68
KasiaTorz <ktorz@wp.pl>
2018-04-09 17:22:39 +0200

    add secound file and create div in posts

---
+++ src/components/Posts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Posts extends Component {
+    render() {
+        return (
+            <div >
+                <h1> Newest posts from fashon world</h1>
+            </div>
+        );
+    }
+}
+
+export default Posts;

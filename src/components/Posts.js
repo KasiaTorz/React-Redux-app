dfa4a1c1fd0332f3bd8389262cd9dd3d20c8ab7a
KasiaTorz <ktorz@wp.pl>
2018-04-09 17:55:51 +0200

    add key id in arrow

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -16,6 +16,7 @@ class Posts extends Component {
             .then(data=>this.setState({posts:data}));
     }
     render() {
+        const postsItems= this.state.posts.map(post=> (<div key={post.id}></div>));
         return (
             <div >
                 <h1> Newest posts from fashon world</h1>

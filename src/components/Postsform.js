63dceba045a4afe1aab1b3edf582e4c744daf3f6
KasiaTorz <ktorz@wp.pl>
2018-04-10 16:58:16 +0200

    create postform component

---
+++ src/components/Postsform.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+class Postsform extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+
+    }
+
+    render() {
+     return(
+            <div >
+                <h1> About Fashon</h1>
+
+            </div>
+        );
+    }
+}
+
+export default Posts;

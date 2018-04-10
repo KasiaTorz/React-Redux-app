f5d1566d1ad56afc2d49111f8b12b31b13b9200a
KasiaTorz <ktorz@wp.pl>
2018-04-10 16:49:59 +0200

    add html elemts h3 and p

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -16,7 +16,11 @@ class Posts extends Component {
             .then(data=>this.setState({posts:data}));
     }
     render() {
-        const postsItems= this.state.posts.map(post=> (<div key={post.id}></div>));
+        const postsItems= this.state.posts.map(post=>
+            (<div key={post.id}>
+                <h3>{post.title} </h3>
+                <p>{post.body}</p>
+            </div>));
         return (
             <div >
                 <h1> Newest posts from fashon world</h1>
